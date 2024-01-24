86737d81b6d3e249d0b3898efdac057b59009dd0
Fruchedes <152749461+Fruchedes@users.noreply.github.com>
2024-01-23 19:28:32 -0600

    Update backend.tf

--- TERRAFORM-JENKINS-CICD-main/backend.tf
+++ TERRAFORM-JENKINS-CICD-main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "zomato"
+    bucket         = "zomato-app1"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "zomato-dynamo-db-table"
